9a2c62d92a855e02a6456be6426ffa4f4ae5845c
binod164 <binod7_grg@hotmail.com>
2022-03-02 17:51:13 -0500

    Create mealSchema

--- models/flight.js
+++ models/flight.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+
 const ticketSchema = new Schema({
   seat: {
     type: String,
@@ -13,7 +14,7 @@ const ticketSchema = new Schema({
     min: 0
   }
 })
-	
+
 const flightSchema = new Schema({
   airline: {
     type:String,
